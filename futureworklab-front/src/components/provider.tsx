a552e89845f3585b0423c531c7689c6d637ea6e5
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-16 07:49:40 +0900

    chore : @tanstack/query import

---
+++ futureworklab-front/src/components/provider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React, { type ReactNode } from 'react';
+import ThemeProvider from '@/components/theme-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+function Providers({ children }: ProviderProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
